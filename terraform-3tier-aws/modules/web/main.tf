16cee3a01a8d57e6248df2be1482d8efe945846c
SATYAM SINGH RAJPOOT <163396283+Rajpootss001@users.noreply.github.com>
2026-03-14 22:10:45 +0530

    Add AWS EC2 instance resource for web server

--- terraform-3tier-aws/modules/web/main.tf
+++ terraform-3tier-aws/modules/web/main.tf
@@ -1,1 +1,9 @@
- 
+ resource "aws_instance" "web" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+  subnet_id     = element(var.public_subnets, 0)
+
+  tags = {
+    Name = "web-server"
+  }
+}
