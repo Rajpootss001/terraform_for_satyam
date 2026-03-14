93ba903b0c5a94b1f05b8f7b012007f905080df8
SATYAM SINGH RAJPOOT <163396283+Rajpootss001@users.noreply.github.com>
2026-03-14 22:08:26 +0530

    Add VPC and application modules to main.tf

--- terraform-3tier-aws/main.tf
+++ terraform-3tier-aws/main.tf
@@ -1,1 +1,26 @@
- 
+ module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr = var.vpc_cidr
+}
+
+module "web" {
+  source = "./modules/web"
+
+  vpc_id = module.vpc.vpc_id
+  public_subnets = module.vpc.public_subnets
+}
+
+module "app" {
+  source = "./modules/app"
+
+  vpc_id = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+}
+
+module "db" {
+  source = "./modules/db"
+
+  vpc_id = module.vpc.vpc_id
+  db_subnets = module.vpc.db_subnets
+}
