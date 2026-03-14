1e191f48cb2fdcd8e6d83f4e881f92791a6dd3ca
SATYAM SINGH RAJPOOT <163396283+Rajpootss001@users.noreply.github.com>
2026-03-14 22:16:52 +0530

    Add AWS instance resource for app server

--- terraform-3tier-aws/modules/app/main.tf
+++ terraform-3tier-aws/modules/app/main.tf
@@ -1,1 +1,9 @@
- 
+ resource "aws_instance" "app" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+  subnet_id     = element(var.private_subnets, 0)
+
+  tags = {
+    Name = "app-server"
+  }
+}
